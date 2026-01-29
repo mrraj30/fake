6b8d2688903c83696a1b0676d56027ca805821ec
Mr. Raj <mr.raj3010@gmail.com>
2026-01-29 15:14:03 +0530

    Fixed Index.html

--- src/index.ts
+++ src/index.ts
@@ -217,7 +217,7 @@ class CardGenerator {
     private async setup(): Promise<void> {
         try {
             // Load SVG Template
-            const svgTemplate = await loadSVG("fake/assets/plain.svg");
+            const svgTemplate = await loadSVG("/assets/plain.svg");
             this.card = new Card(svgTemplate);
 
             // Clear container and inject SVG
